ac748f5cccd3fd20707fdb18f40c85d44773e100
seungo ha <seungo.ha@actionpower.kr>
2024-02-16 09:35:04 +0900

    feat: qualifier and primary

--- src/main/java/spring/core/discount/FixDiscountPolicy.java
+++ src/main/java/spring/core/discount/FixDiscountPolicy.java
@@ -1,10 +1,12 @@
 package spring.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 
 @Component
+@Qualifier("subDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private final int discountFixAmount = 1000; // 1000원 할인
