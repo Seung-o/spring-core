5954526c1eae51f9d055a40be417f807a6d8654c
seungo <ilkhso@gmail.com>
2024-02-18 22:33:19 +0900

    feat: java init and destroy callback method on bean

---
+++ src/test/java/spring/core/lifecycle/NetworkClient.java
@@ -0,0 +1,42 @@
+package spring.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    private void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+}
